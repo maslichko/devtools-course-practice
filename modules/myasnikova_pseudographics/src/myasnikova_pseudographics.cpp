9497045cf88fada98f08f8e7f6f74ac7a424af3d
var2var3varvara_h <varyam6@gmail.com>
2022-04-17 15:06:59 +0300

    v0

---
+++ modules/myasnikova_pseudographics/src/myasnikova_pseudographics.cpp
@@ -0,0 +1,5 @@
+// Copyright 2022 Myasnikova Varvara
+
+#include "include/myasnikova_pseudographics.h"
+
+//Код
---
+++ modules/myasnikova_pseudographics/test/test_pseudographics.cpp
@@ -0,0 +1,8 @@
+// Copyright 2022 Myasnikova Varvara
+
+#include <gtest/gtest.h>
+
+#include <tuple>
+#include "include/myasnikova_pseudographics.h"
+
+//Тесты
