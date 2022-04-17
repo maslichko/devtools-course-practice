c050c9f9723ee8f53675612246ee53f5e17cd9be
var2var3varvara_h <varyam6@gmail.com>
2022-04-17 16:36:11 +0300

    v1

--- modules/myasnikova_pseudographics/include/myasnikova_pseudographics.h
+++ modules/myasnikova_pseudographics/include/myasnikova_pseudographics.h
@@ -1,49 +1,25 @@
-// Copyright 2017 Korniakov Kirill
+// Copyright 2022 Myasnikova Varvara
 
-#ifndef MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_CALCULATOR_H_
-#define MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_CALCULATOR_H_
+#ifndef MODULES_MYASNIKOVA_PSEUDOGRAPHICS_INCLUDE_MYASNIKOVA_PSEUDOGRAPHICS_H
+#define MODULES_MYASNIKOVA_PSEUDOGRAPHICS_INCLUDE_MYASNIKOVA_PSEUDOGRAPHICS_H
 
 #include <string>
 
-/** @defgroup complex-number Complex Numbers Algebra
-  @{
-    This module contains:
-        - A definition of complex number as a class
-        - Calculator class which performs the calculations
-  @}
-*/
-
-//! @addtogroup complex-number
-//! @{
-
-/**
- * @brief Class for complex numbers algebra
- */
-class ComplexCalculator {
+class Pseudographics {
  public:
-    ComplexCalculator();
-
-    /**
-     * @brief Functor override
-     * @param[in] argc Nuber of comand line arguments
-     * @param[in] argv Command line arguments
-     * @return Output expression
-     */
-    std::string operator()(int argc, const char** argv);
-
+    Pseudographics();
+    void PrintPseudographics(int num);
  private:
-    void help(const char* appname, const char* message = "");
-    bool validateNumberOfArguments(int argc, const char** argv);
-    std::string message_;
-    typedef struct {
-        double z1_real;
-        double z1_imaginary;
-        double z2_real;
-        double z2_imaginary;
-        char operation;
-    } Arguments;
+   std::string cout_0[3];
+   std::string cout_1[3];
+   std::string cout_2[3];
+   std::string cout_3[3];
+   std::string cout_4[3];
+   std::string cout_5[3];
+   std::string cout_6[3];
+   std::string cout_7[3];
+   std::string cout_8[3];
+   std::string cout_9[3];
 };
 
-//! @}
-
-#endif  // MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_CALCULATOR_H_
+#endif  // MODULES_MYASNIKOVA_PSEUDOGRAPHICS_INCLUDE_MYASNIKOVA_PSEUDOGRAPHICS_H
--- modules/myasnikova_pseudographics/test/test_pseudographics.cpp
+++ modules/myasnikova_pseudographics/test/test_pseudographics.cpp
@@ -5,4 +5,54 @@
 #include <tuple>
 #include "include/myasnikova_pseudographics.h"
 
+TEST(Pseudographics, CanPrint_1) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(1));
+}
+
+TEST(Pseudographics, CanPrint_2) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(2));
+}
+
+TEST(Pseudographics, CanPrint_3) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(3));
+}
+
+TEST(Pseudographics, CanPrint_4) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(4));
+}
+
+TEST(Pseudographics, CanPrint_5) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(5));
+}
+
+TEST(Pseudographics, CanPrint_6) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(6));
+}
+
+TEST(Pseudographics, CanPrint_7) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(7));
+}
+
+TEST(Pseudographics, CanPrint_8) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(8));
+}
+
+TEST(Pseudographics, CanPrint_9) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(9));
+}
+
+TEST(Pseudographics, CanPrint_0) {
+  Pseudographics p;
+  ASSERT_NO_THROW(p.PrintPseudographics(0));
+}
+
 //Тесты
